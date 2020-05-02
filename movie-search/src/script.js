473d635540ed0f8a13559b683cc01e099f20cf84
ALEX\alex <lyssenko.alex@gmail.com>
2020-05-02 19:44:01 +0600

    feat: virtual slide

--- movie-search/src/script.js
+++ movie-search/src/script.js
@@ -5,99 +5,70 @@ import { LANGUAGE } from './constraints';
 let CURRENT_PAGE = 1;
 
 const init = async () => {
-	let movies = await getOMDBInfo({title:'rabbit', page:CURRENT_PAGE});
-	await fillSliders(movies);
+	let movies = await getOMDBInfo({ title: 'rabbit', page: CURRENT_PAGE });
 	await startSlider(movies);
 };
 
 const fillSliders = (movieArray) => {
 	if (movieArray.Response === 'True') {
-		console.log('movieArray', movieArray)
+		console.log('movieArray', movieArray);
 		movieArray.Search.forEach((x) => {
-			createSlide(x,);
+			createSlide(x);
 		});
 	}
 };
 
 const startSlider = async (movies) => {
-	console.log('movies: ', movies)
+	console.log('movies: ', movies.Search);
 	let swiper = new Swiper('.swiper-container', {
 		slidesPerView: 5,
 		spaceBetween: 50,
-		updateOnWindowResize:true,
+		updateOnWindowResize: true,
 		pagination: {
 			el: '.swiper-pagination',
 			clickable: true,
-			dynamicBullets:true,
-			dynamicMainBullets:true,
-			type:'fraction',
+			dynamicBullets: true,
+			dynamicMainBullets: true,
+			type: 'fraction',
 			renderFraction: function (currentClass, totalClass) {
 				console.log('totalClass', totalClass);
-				return '<span class="' + currentClass + '"></span>' +
-					' of ' +
-					'<span class="' + totalClass + '"></span>';
+				return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
 			}
 		},
 		navigation: {
 			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+			prevEl: '.swiper-button-prev'
 		},
 		virtual: {
 			slides: (function () {
-				let slides = [];
-				for (let i = 0; i < movies.totalResults; i += 1) {
-					slides.push('Slide ' + (i + 1));
-				}
-				return slides;
-			}()),
-		},
+				return createSlides(movies);
+			})()
+		}
 	});
 	let y = swiper.slideNext();
-	swiper.on('reachEnd', () => {
-		console.log('reachEnd')
-		loadNextSlides(CURRENT_PAGE++)
-	})
-	// let t = await getYandexTranslateURL({ text: 'dream', from_lang: LANGUAGE.ENG.VALUE, to_lang: LANGUAGE.RU.VALUE });
-	// console.log('y', y);
+	swiper.on('reachEnd', async () => {
+		let movies = await getOMDBInfo({ title: 'rabbit', page: ++CURRENT_PAGE });
+		swiper.virtual.appendSlide(createSlides(movies));
+	});
 };
 
 const loadNextSlides = async (page) => {
-	let movies = await getOMDBInfo({title:'rabbit', page:page});
+	let movies = await getOMDBInfo({ title: 'rabbit', page: page });
 	await fillSliders(movies);
-}
+};
 
-const createSlide = (movie) => {
+const createSlides = (movies) => {
+	let slides = [];
 
-	let el = document.querySelector('.swiper-wrapper');
+    for (let i = 0; i < movies.Search.length; i += 1) {
+        slides.push(`
+                            <div class="card">
+                            <img src="${movies.Search[i].Poster}" alt="${movies.Search[i].Title}"/>
+                            <h5>${movies.Search[i].Title}</h5>
+                            </div>
+                                `);
+    }
+    return slides;
 
-	let sliderDiv = document.createElement('div');
-	sliderDiv.classList.add('swiper-slide');
-
-	let card = document.createElement('div');
-	card.classList.add('card');
-
-	let img = document.createElement('IMG');
-	img.setAttribute('src', movie.Poster);
-	img.setAttribute('alt', movie.Title);
-	card.append(img);
-
-	let cardBody = document.createElement('div');
-	cardBody.classList.add('card-body');
-
-	let title = document.createElement('h5');
-	title.classList.add('card-title');
-	title.innerText = movie.Title;
-	cardBody.append(title);
-
-	// let subTitle = document.createElement('h5');
-	// subTitle.classList.add('card-subtitle');
-	// subTitle.innerText = 'card-subtitle';
-	// cardBody.append(subTitle);
-
-	// let button = document.createElement('button');
-	// cardBody.append(button);
-	card.append(cardBody);
-	sliderDiv.append(card);
-	el.append(sliderDiv);
 };
 init();
