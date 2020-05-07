dcbf8f95409a941fbb5f5b6c59fd8e03df3bc078
ALEX\alex <lyssenko.alex@gmail.com>
2020-05-07 21:06:30 +0600

    feat: some responsive

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -107,6 +107,10 @@ const startSlider = async (movies, title) => {
 				slidesPerView: 2,
 				spaceBetween: 20
 			},
+			720: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			},
 			768: {
 				slidesPerView: 4,
 				spaceBetween: 40
