15f2111fbaff2d8c972d3eb46357def8a84a3892
ALEX\alex <lyssenko.alex@gmail.com>
2020-05-08 21:48:32 +0600

    refactor: css fix

--- movie-search/webpack.config.js
+++ movie-search/webpack.config.js
@@ -1,65 +1,70 @@
 const webpack = require('webpack');
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, options) => {
-	const isProduction = options.mode === 'production';
+    const isProduction = options.mode === 'production';
 
-	return {
-		mode: isProduction ? 'production' : 'development',
-		devtool: isProduction ? 'none' : 'source-map',
-		watch: !isProduction,
-		entry: ['@babel/polyfill', './src/index.js', './src/sass/style.scss'],
-		output: {
-			path: path.join(__dirname, '/dist'),
-			filename: 'script.js'
-		},
+    return {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'source-map',
+        watch: !isProduction,
+        entry: ['@babel/polyfill', './src/index.js', './src/sass/style.scss'],
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'script.js'
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'all',
+            },
+        },
 
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env']
-						}
-					}
-				},
-				{
-					test: /\.scss$/,
-					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-				},
-				{
-					test: /\.(png|svg|jpe?g|gif|woff|woff2|ttf)$/,
-					use: [
-						{
-							loader: 'file-loader'
-						}
-					]
-				},
-				{
-					test: /\.html$/,
-					loader: 'html-loader'
-				},
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(png|svg|jpe?g|gif|woff|woff2|ttf)$/,
+                    use: [
+                        {
+                            loader: 'file-loader'
+                        }
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'html-loader'
+                },
 
-			]
-		},
-		devServer: {
-			contentBase: './dist'
-		},
-		plugins: [
-			new CleanWebpackPlugin(),
-			new HtmlWebpackPlugin({
-				template: 'index.html',
-				favicon: "./favicon/Treetog-I-Videos.ico"
-			}),
-			new MiniCssExtractPlugin({
-				filename: 'style.css'
-			})
-		]
-	};
+            ]
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.html',
+                favicon: "./favicon/Treetog-I-Videos.ico"
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
+        ]
+    };
 };
