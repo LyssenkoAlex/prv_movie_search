93834debab8cb3be9d163bae9ecaffadab33ade8
ALEX\alex <lyssenko.alex@gmail.com>
2020-05-09 15:12:13 +0600

    refactor: eslint fix errors

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -12,7 +12,6 @@ const fountTotal = document.querySelector('.fondWrapper');
 const clearButton = document.querySelector('.clear_icon');
 
 const loader = document.querySelector('.loader');
-const not_found = document.querySelector('.not_found');
 const searchNote = document.querySelector('.searchNote');
 
 const loaderUpdate = (loaderCommand) => {
@@ -30,16 +29,17 @@ const loaderUpdate = (loaderCommand) => {
 };
 
 const fillImdRating = async (movies) => {
-	let ratingURl = movies.Search.map((movie) => OMDB_URL + 'i=' + movie.imdbID);
+	const ratingURl = movies.Search.map((movie) => `${OMDB_URL}i=${movie.imdbID}`);
 	try {
-		let ratingResp = await Promise.all(
+		const ratingResp = await Promise.all(
 			ratingURl.map(async (rating) => {
-				let response = await fetch(rating);
+				const response = await fetch(rating);
 				return response.json();
 			})
 		);
 
 		movies.Search.forEach((movie) => {
+			// eslint-disable-next-line no-param-reassign
 			movie.imdbRating = ratingResp.filter((rating) => rating.imdbID === movie.imdbID)[0].imdbRating;
 		});
 	} catch (e) {
@@ -48,39 +48,56 @@ const fillImdRating = async (movies) => {
 	}
 };
 
-const init = async (title) => {
-	loaderUpdate('start');
-	let movies = await getOMDBInfo({ title: title, page: CURRENT_PAGE });
-	if (movies.Search.length > 0) {
+const createSlides = (movies) => {
+	const slides = [];
+	for (let i = 0; i < movies.Search.length; i += 1) {
+		slides.push(`
+				<div class="swiper-slide">
+					<div class="card">
+					<img src="${movies.Search[i].Poster}" alt="${movies.Search[i].Title}"/>
+					<div class="card-body">
+						<a class="paper-btn btn-secondary" href="https://www.imdb.com/title/${movies.Search[i].imdbID}" target="_blank">${movies.Search[i].Title}</a>
+						<h5 class="card-subtitle">${movies.Search[i].Type} ${movies.Search[i].Year}</h5>
+						<h5 class="card-subtitle">Rating: ${movies.Search[i].imdbRating}</h5>
+					</div>
+					</div>
+	            </div>
+	                `);
+	}
+	return slides;
+};
+
+const searchTitle = async (searchType) => {
+	loaderUpdate('none', 'block');
+	// eslint-disable-next-line no-plusplus
+	searchType === 'PROCEED' ? ++CURRENT_PAGE : (CURRENT_PAGE = 1);
+	let title = '';
+	inputTitle.value === '' ? (title = 'rabbit') : (title = inputTitle.value);
+	const translation = await getYandexTranslateURL({ text: title });
+	const translatedWord = translation.text[0];
+	const movies = await getOMDBInfo({ title: translatedWord, page: CURRENT_PAGE });
+	if (movies.Search !== undefined && movies.Search.length > 0) {
+		loaderUpdate('start');
 		await fillImdRating(movies);
-		await startSlider(movies, title);
-		cursorPosition();
+		if (searchType === 'NEW') {
+			swiper.virtual.slides = [];
+			swiper.virtual.slides = createSlides(movies);
+			swiper.virtual.update(true);
+			swiper.slideTo(0, 1);
+		}
+
+		if (searchType === 'PROCEED') {
+			swiper.virtual.appendSlide(createSlides(movies));
+			swiper.virtual.update(true);
+		}
 		fountTotal.innerHTML = `Found: ${movies.totalResults}`;
 		loaderUpdate('stop');
 		searchNote.innerHTML = `Showing results for ${title}`;
 	} else {
-		not_found.innerHTML = 'Nothing found!';
+		fountTotal.innerHTML = 'Nothing found!';
+		loaderUpdate('error');
+		searchNote.innerHTML = `Nothing found for ${title}`;
 	}
-};
-const cursorPosition = () => {
-	const setCaretPosition = (ctrl, pos) => {
-		// Modern browsers
-		if (ctrl.setSelectionRange) {
-			ctrl.focus();
-			ctrl.setSelectionRange(pos, pos);
-
-			// IE8 and below
-		} else if (ctrl.createTextRange) {
-			let range = ctrl.createTextRange();
-			range.collapse(true);
-			range.moveEnd('character', pos);
-			range.moveStart('character', pos);
-			range.select();
-		}
-	};
-
-	let input = document.getElementById('paperInputs1');
-	setCaretPosition(input, input.value.length);
 };
 
 const startSlider = async (movies, title) => {
@@ -145,81 +162,66 @@ const startSlider = async (movies, title) => {
 	});
 };
 
-const createSlides = (movies) => {
-	let slides = [];
-	for (let i = 0; i < movies.Search.length; i += 1) {
-		slides.push(`
-				<div class="swiper-slide">
-					<div class="card">
-					<img src="${movies.Search[i].Poster}" alt="${movies.Search[i].Title}"/>
-					<div class="card-body">
-						<a class="paper-btn btn-secondary" href="https://www.imdb.com/title/${movies.Search[i].imdbID}" target="_blank">${movies.Search[i].Title}</a>
-						<h5 class="card-subtitle">${movies.Search[i].Type} ${movies.Search[i].Year}</h5>
-						<h5 class="card-subtitle">Rating: ${movies.Search[i].imdbRating}</h5>
-					</div>
-					</div>
-	            </div>
-	                `);
-	}
-	return slides;
+const cursorPosition = () => {
+	const setCaretPosition = (ctrl, pos) => {
+		// Modern browsers
+		if (ctrl.setSelectionRange) {
+			ctrl.focus();
+			ctrl.setSelectionRange(pos, pos);
+
+			// IE8 and below
+		} else if (ctrl.createTextRange) {
+			const range = ctrl.createTextRange();
+			range.collapse(true);
+			range.moveEnd('character', pos);
+			range.moveStart('character', pos);
+			range.select();
+		}
+	};
+
+	const input = document.getElementById('paperInputs1');
+	setCaretPosition(input, input.value.length);
 };
-init('rabbit');
 
-const searchTitle = async (searchType) => {
-	loaderUpdate('none', 'block');
-	searchType === 'PROCEED' ? ++CURRENT_PAGE : (CURRENT_PAGE = 1);
-	let title = '';
-	inputTitle.value === '' ? (title = 'rabbit') : (title = inputTitle.value);
-	let translation = await getYandexTranslateURL({ text: title });
-	let translatedWord = translation.text[0];
-	let movies = await getOMDBInfo({ title: translatedWord, page: CURRENT_PAGE });
-	if (movies.Search !== undefined && movies.Search.length > 0) {
-		loaderUpdate('start');
+const init = async (title) => {
+	loaderUpdate('start');
+	const movies = await getOMDBInfo({ title, page: CURRENT_PAGE });
+	if (movies.Search.length > 0) {
 		await fillImdRating(movies);
-		if (searchType === 'NEW') {
-			swiper.virtual.slides = [];
-			swiper.virtual.slides = createSlides(movies);
-			swiper.virtual.update(true);
-			swiper.slideTo(0, 1);
-		}
-
-		if (searchType === 'PROCEED') {
-			swiper.virtual.appendSlide(createSlides(movies));
-			swiper.virtual.update(true);
-		}
+		await startSlider(movies, title);
+		cursorPosition();
 		fountTotal.innerHTML = `Found: ${movies.totalResults}`;
 		loaderUpdate('stop');
 		searchNote.innerHTML = `Showing results for ${title}`;
 	} else {
-		not_found.innerHTML = 'Nothing found!';
-		fountTotal.innerHTML = '0';
-		loaderUpdate('error');
-		searchNote.innerHTML = `Nothing found for ${title}`;
+		fountTotal.innerHTML = 'Nothing found!';
 	}
 };
+
+init('rabbit');
 
 inputTitle.addEventListener('keypress', async (e) => {
 	if (e.key === 'Enter') {
 		try {
 			await searchTitle('NEW');
-		} catch (e) {
+		} catch (error) {
 			loaderUpdate('error');
 			searchNote.innerHTML = `Error has happened`;
 		}
 	}
 });
 
-buttonSearch.addEventListener('click', async (e) => {
+buttonSearch.addEventListener('click', async () => {
 	CURRENT_PAGE = 1;
 	try {
 		await searchTitle('NEW');
-	} catch (e) {
+	} catch (error) {
 		loaderUpdate('error');
 		searchNote.innerHTML = `Error has happened`;
 	}
 });
 
-inputTitle.addEventListener('keyup', (e) => {
+inputTitle.addEventListener('keyup', () => {
 	clearButton.style.visibility = inputTitle.value.length ? 'visible' : 'hidden';
 });
 
